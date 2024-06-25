2d1566099479fa3013f5ae7a2925c190cd3e9486
avila-ca <avilacalaf@gmail.com>
2024-06-25 21:15:40 +0200

    changing paths to deploy in netlify

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Col, Container, Nav, Navbar, Image, Row } from "react-bootstrap"
 import { Props } from "./Footer";
+import { Link } from "react-router-dom";
 
 export const Header = (props:Props) => {
    
@@ -10,15 +11,15 @@ export const Header = (props:Props) => {
                     <Row className="w-100 justify-content-between align-items-center">
                         <Col xs="auto"> 
                             <div className="d-flex align-items-center">
-                                <Image className="w-25" src="/src/img/logo/logo.png" />
+                                <Image className="w-25" src={`${process.env.PUBLIC_URL}/src/img/logo/logo.png`}/>
                                 <Navbar.Brand href="#home" className="ms-2">{props.label}</Navbar.Brand>
                             </div>
                         </Col>
                         <Col  xs="auto">
                             <Nav className="me-auto justify-content-end">
-                                <Nav.Link href="/">Inicio</Nav.Link>
-                                <Nav.Link href="/projects">Proyectos</Nav.Link>
-                                <Nav.Link href="/contact">Contacto</Nav.Link>
+                                <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+                                <Nav.Link as={Link} to="/projects">Proyectos</Nav.Link>
+                                <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
                             </Nav>
                         </Col>
                     </Row>
