c8293d2ed6cc8ec0adf98957e27026ded18fda97
avila-ca <avilacalaf@gmail.com>
2024-07-04 19:40:43 +0200

    Insert services page and their cards

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
 import { Col, Container, Nav, Navbar, Image, Row } from "react-bootstrap"
 import { Props } from "./Footer";
 import { Link } from "react-router-dom";
-import logo from '../img/logo/logo.png'
-import logo2 from '../img/logo/REFORMA INTEGRAL.png'
+import logo2 from '../img/logo/REFORMA_INTEGRAL-removebg-preview-removebg-preview.png'
 export const Header = (props:Props) => {
    
     return (
@@ -12,16 +11,15 @@ export const Header = (props:Props) => {
                     <Row className="w-100 justify-content-between align-items-center">
                         <Col xs="auto"> 
                             <div className="d-flex align-items-center">
-                            <Image className="w-25" src={logo2}/>
-
-                                <Image className="w-25" src={logo}/>
-                                <Navbar.Brand href="#home" className="ms-2">{props.label}</Navbar.Brand>
+                                <Image className="w-25 " src={logo2}/>
+                                <Navbar.Brand href="#home" className="ms-2" style={{fontFamily: "Nimbus Sans"}} >{props.label}</Navbar.Brand>
                             </div>
                         </Col>
                         <Col xs="auto">
                             <Nav className="me-auto justify-content-end">
                                 <Nav.Link as={Link} to="/">Inicio</Nav.Link>
                                 <Nav.Link as={Link} to="/projects">Proyectos</Nav.Link>
+                                <Nav.Link as={Link} to="/servicios">Servicios</Nav.Link>
                                 <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
                             </Nav>
                         </Col>
